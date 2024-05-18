9878202120fff84efcc264821fad66883cf0dc07
Veronika Korolkova <veronika.korolkova99@gmail.com>
2024-05-18 13:25:51 +0700

    [task 1, stage 4] Вынесла проверку одной задачи в отдельную функцию, поменяла проверку на массив

--- ReactTasks/task-1/stage-4/src/data/dataTasks.ts
+++ ReactTasks/task-1/stage-4/src/data/dataTasks.ts
@@ -46,22 +46,29 @@ export const prepareDataTasks = (data: Array<Task>) => {
     .sort(sortByCardOrder);
 };
 
+export const isTask = (task: unknown): task is Task => {
+  if (
+    task instanceof Object &&
+    'id' in task &&
+    typeof task.id === 'string' &&
+    'order' in task &&
+    typeof task.order === 'number' &&
+    'title' in task &&
+    typeof task.title === 'string' &&
+    'description' in task &&
+    typeof task.description === 'string'
+  ) {
+    return true;
+  }
+  return false;
+};
+
 export const isTasks = (tasks: unknown): tasks is Task[] => {
-  if (!(tasks instanceof Array) || tasks.length === 0) {
+  if (!Array.isArray(tasks)) {
     return false;
   }
   for (const task of tasks) {
-    if (
-      !(task instanceof Object) ||
-      !('id' in task) ||
-      typeof task.id !== 'string' ||
-      !('order' in task) ||
-      typeof task.order !== 'number' ||
-      !('title' in task) ||
-      typeof task.title !== 'string' ||
-      !('description' in task) ||
-      typeof task.description !== 'string'
-    ) {
+    if (!isTask(task)) {
       return false;
     }
   }
