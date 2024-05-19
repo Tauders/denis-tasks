4f338c052364f7754d768c9f5967b3ac758a2cc5
Veronika Korolkova <veronika.korolkova99@gmail.com>
2024-05-19 23:04:21 +0700

    [SubtasksJS-6] Поменяла общий экспорт на экспорты "на месте"

--- SubtasksJs/Nika/SubtaskJS-6/src/js/const.js
+++ SubtasksJs/Nika/SubtaskJS-6/src/js/const.js
@@ -1,24 +1,24 @@
-const blockControlID = 'control';
-const blockControl = document.getElementById(blockControlID);
-const buttonHandleID = 'handle';
-const buttonHandle = document.getElementById(buttonHandleID);
-const buttonSendID = 'send';
-const formResultID = 'result';
-const formResult = document.getElementById(formResultID);
-const buttonClassName = 'form__button';
-const blockErrorID = 'error';
-const blockError = document.getElementById(blockErrorID);
-const buttonLinkID = 'download';
-const buttonLink = document.getElementById(buttonLinkID);
-const classInputError = 'form__input_error';
-const inputSelectFileID = 'selectFile';
-const inputSelectFile = document.getElementById(inputSelectFileID);
-const inputFileTextID = 'selectFileText';
-const inputFileText = document.getElementById(inputFileTextID);
-const EMAIL_REGEXP =
+export const blockControlID = 'control';
+export const blockControl = document.getElementById(blockControlID);
+export const buttonHandleID = 'handle';
+export const buttonHandle = document.getElementById(buttonHandleID);
+export const buttonSendID = 'send';
+export const formResultID = 'result';
+export const formResult = document.getElementById(formResultID);
+export const buttonClassName = 'form__button';
+export const blockErrorID = 'error';
+export const blockError = document.getElementById(blockErrorID);
+export const buttonLinkID = 'download';
+export const buttonLink = document.getElementById(buttonLinkID);
+export const classInputError = 'form__input_error';
+export const inputSelectFileID = 'selectFile';
+export const inputSelectFile = document.getElementById(inputSelectFileID);
+export const inputFileTextID = 'selectFileText';
+export const inputFileText = document.getElementById(inputFileTextID);
+export const EMAIL_REGEXP =
   /^(([^&lt;&gt;()\[\]\\.,;:\s@"]+(\.[^&lt;&gt;()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-const NAME_REGEXP = /^[a-zA-Zа-яА-ЯЁё]+$/;
-const error = {
+export const NAME_REGEXP = /^[a-zA-Zа-яА-ЯЁё]+$/;
+export const error = {
   file: { id: 'error-file-type', text: 'Требуемый тип файла "Json' },
   firstname: {
     id: 'error-incorrect-firstname',
@@ -31,28 +31,8 @@ const error = {
   email: { id: 'error-incorrect-email', text: 'Неккоректный адрес email' },
   phone: { id: 'error-incorrect-phone', text: 'Неккоректный номер телефона' },
 };
-const visibleFormClassName = 'form_visible';
-const visibleErrorClassName = 'form__error_visible';
-const errorTextClassName = 'form__error-text';
-const initialContentOfResultBlock =
+export const visibleFormClassName = 'form_visible';
+export const visibleErrorClassName = 'form__error_visible';
+export const errorTextClassName = 'form__error-text';
+export const initialContentOfResultBlock =
   '<h2 class="form__title">Результат генерации</h2>';
-
-export {
-  blockControl,
-  buttonHandle,
-  formResult,
-  buttonClassName,
-  blockError,
-  buttonLink,
-  classInputError,
-  EMAIL_REGEXP,
-  NAME_REGEXP,
-  error,
-  inputSelectFile,
-  inputFileText,
-  visibleFormClassName,
-  visibleErrorClassName,
-  errorTextClassName,
-  initialContentOfResultBlock,
-  buttonSendID
-};
