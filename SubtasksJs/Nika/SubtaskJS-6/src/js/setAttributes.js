a2969f16de6964f99e2b488969219068a1766a5e
Veronika <veronika.korolkova99@gmail.com>
2024-05-30 18:46:10 +0700

    [SubtasksJS-6] Изменила проверки для атрибутов.

--- SubtasksJs/Nika/SubtaskJS-6/src/js/setAttributes.js
+++ SubtasksJs/Nika/SubtaskJS-6/src/js/setAttributes.js
@@ -1,14 +1,14 @@
 export function setAttributes(element, objectAttributes) {
   const arrayAttributes = Object.entries(objectAttributes);
   for (const [key, value] of arrayAttributes) {
-    if (Array.isArray(value)) {
+    if (key === 'options' && Array.isArray(value)) {
       value.map(item => {
         const option = document.createElement('option');
         setAttributes(option, item);
         option.innerText = item.name;
         element.append(option);
       });
-    } else if (key === 'required') {
+    } else if (typeof value === 'boolean') {
       if (value === true) {
         element.setAttribute(key, '');
       }
