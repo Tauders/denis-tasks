64b12134c390a3525f090d731322a4653feabbaa
Veronika <veronika.korolkova99@gmail.com>
2024-06-21 02:04:57 +0700

    [task 1, stage 6] Добавила компонент для тэгов

---
+++ ReactTasks/task-1/stage-6/src/pages/TodoLists/components/Tags/Tags.tsx
@@ -0,0 +1,59 @@
+import { Button } from '../../../../components/Button/Button';
+import { Modal } from '../../../../components/Modal/Modal';
+import { useModal } from '../../../../hooks/useModal';
+import { Task } from '../../../../ts/types';
+import classes from './Tags.module.scss';
+import { useState } from 'react';
+
+type TagsProps = {
+  card: Task;
+  onDeleteTagToCard: (card: Task) => void;
+};
+
+export const Tags = (props: TagsProps) => {
+  const { card, onDeleteTagToCard } = props;
+  const { isOpen, handleOpenModal, handleCloseModal } = useModal(false);
+  const [currentTag, setCurrentTag] = useState('');
+
+  const handleClickTag = (tag: string) => {
+    handleOpenModal();
+    setCurrentTag(tag);
+  };
+
+  const handleDeleteTag = () => {
+    const updatedCard = {
+      ...card,
+      tags: card.tags && card.tags.filter(tag => tag !== currentTag),
+    };
+    onDeleteTagToCard(updatedCard);
+    handleCloseModal();
+  };
+
+  return (
+    <>
+      <Modal
+        isOpen={isOpen}
+        onCloseModal={handleCloseModal}
+        title="Delete tag?"
+      >
+        <p>Are you sure you want to remove the tag?</p>
+        <Button onClick={handleDeleteTag} className={classes.tags__modalButton}>
+          Yes
+        </Button>
+      </Modal>
+      <ul className={classes.tags}>
+        {card.tags &&
+          card.tags.map(tag => {
+            return (
+              <li
+                className={classes.tags__tag}
+                onClick={() => handleClickTag(tag)}
+              >
+                {tag}
+              </li>
+            );
+          })}
+      </ul>
+    </>
+  );
+};
